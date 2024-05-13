1e4ac744eba109204c3bf26abda6ea3745cf61fb
KimRiun <56223389+KimRiun@users.noreply.github.com>
2024-05-13 09:38:36 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-      {/* <CountComponent/> */}
+import logo from './logo.svg';
+import { useState } from 'react';
 
-      {/* <PrimeCalculator/> */}
-      
-      {/* <FocusInputButton /> */}
+function App() {
+  return (
+    <div className="App">
+      <TodoApp />
+    </div>
+  );
+}
 
-
-      {/* <button onClick={e=>{
-        setShowCount(!showCount);
-      }}>버튼</button>
-      {showCount ? <CountComponent/> : null} */}
-      {/* <BlinkComponent text="이건 깜빡입니다"/> */}
-      <div>
-        {/* <HelloWorld/> */}
-        {/* <CountComponent/>
-        <BlinkComponent text="블링블링"/>
-        <CBlinkComponent text="CCCCC블링블링"/>
-        <CaptionImage
-          imgUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrggwhBlIIyNBOQfllbvJsDS9AlI7mtMStKS95dLrrWw&s"
-          caption="구름 그림"
-        />
-        <CaptionImage
-          imgUrl="https://i.namu.wiki/i/VM69YHBrOvWQc_MqYzlX1yqmhfiImMD7yBCHtlJffRYjgm5njyrnMnn6gOH7A1emxNAOKh3OQqdyFWggbOD7rdN3M0iEr8gwUbS8qXffH5--eLMfgmetiSWs8ygv7AfLJvWqLoADE1xHbizhCuYMbA.webp"
-          caption="이건 트럭입니다"
-        /> */}
-      </div>
+export default App;
